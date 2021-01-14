38eb8b740c8ee44a466c8499013341c5d5be44d9
veluvj <veluvijay1804@gmail.com>
2021-01-14 12:55:49 +0530

    code alignment

--- main.js
+++ main.js
@@ -11,48 +11,48 @@ var sideBar = document.getElementById('sideBar');
 var body = document.getElementById('main');
 var inner = document.createElement('div');
 
-inner.innerHTML =   '<img src="Assets/images/pexels-cottonbro-4761352.jpg">' +
-                    '<img src="Assets/images/pexels-cottonbro-4761352.jpg">' +
-                    '<img src="Assets/images/pexels-cottonbro-4761352.jpg">' +
-                    '<img src="Assets/images/pexels-cottonbro-4761352.jpg">' +
-                    '<img src="Assets/images/pexels-cottonbro-4761352.jpg">' +
-                    '<img src="Assets/images/pexels-cottonbro-4761352.jpg">' +
-                    '<img src="Assets/images/pexels-cottonbro-4761352.jpg">' +
-                    '<img src="Assets/images/pexels-cottonbro-4761352.jpg">' +
-                    '<img src="Assets/images/pexels-cottonbro-4761352.jpg">' +
-                    '<img src="Assets/images/pexels-cottonbro-4761352.jpg">' +
-                    '<img src="Assets/images/pexels-cottonbro-4761352.jpg">' +
-                    '<img src="Assets/images/pexels-cottonbro-4761352.jpg">' +
-                    '<img src="Assets/images/pexels-cottonbro-4761352.jpg">' +
-                    '<img src="Assets/images/pexels-cottonbro-4761352.jpg">' ;
+inner.innerHTML = '<img src="Assets/images/pexels-cottonbro-4761352.jpg">' +
+    '<img src="Assets/images/pexels-cottonbro-4761352.jpg">' +
+    '<img src="Assets/images/pexels-cottonbro-4761352.jpg">' +
+    '<img src="Assets/images/pexels-cottonbro-4761352.jpg">' +
+    '<img src="Assets/images/pexels-cottonbro-4761352.jpg">' +
+    '<img src="Assets/images/pexels-cottonbro-4761352.jpg">' +
+    '<img src="Assets/images/pexels-cottonbro-4761352.jpg">' +
+    '<img src="Assets/images/pexels-cottonbro-4761352.jpg">' +
+    '<img src="Assets/images/pexels-cottonbro-4761352.jpg">' +
+    '<img src="Assets/images/pexels-cottonbro-4761352.jpg">' +
+    '<img src="Assets/images/pexels-cottonbro-4761352.jpg">' +
+    '<img src="Assets/images/pexels-cottonbro-4761352.jpg">' +
+    '<img src="Assets/images/pexels-cottonbro-4761352.jpg">' +
+    '<img src="Assets/images/pexels-cottonbro-4761352.jpg">';
 
 flexBox.classList.add('flex-box');
 
-var scrollAnimation = function(val){
-    for( var idx=1; idx<=10; idx++ ){
-        setTimeout(function(){
+var scrollAnimation = function (val) {
+    for (var idx = 1; idx <= 10; idx++) {
+        setTimeout(function () {
             flexBox.scrollLeft = flexBox.scrollLeft + val + idx;
-        }, 50*idx);
+        }, 50 * idx);
     }
 }
 
-leftBtn.onclick= function(){
-    if( window.innerWidth > 900) {
+leftBtn.onclick = function () {
+    if (window.innerWidth > 900) {
         scrollAnimation(-50);
     } else {
         scrollAnimation(-30)
     }
 };
 
-rightBtn.onclick= function(){
-    if( window.innerWidth > 900) {
+rightBtn.onclick = function () {
+    if (window.innerWidth > 900) {
         scrollAnimation(30);
     } else {
         scrollAnimation(10)
     }
 };
 
-var mobMenu = function(e){
+var mobMenu = function (e) {
     if (!e) var e = window.event;
     e.cancelBubble = true;
     if (e.stopPropagation) e.stopPropagation();
@@ -60,8 +60,8 @@ var mobMenu = function(e){
     sideBar.style.right = "0px"
 }
 
-body.onclick = function(){
-    if(sideBar.style.display == "block" && sideBar.style.right == "0px"){
+body.onclick = function () {
+    if (sideBar.style.display == "block" && sideBar.style.right == "0px") {
         sideBar.style.display = "none";
         sideBar.style.right = "-100px"
     } else {
@@ -70,21 +70,7 @@ body.onclick = function(){
 }
 
 
-
-// window.addEventListener('click', function(){
-//     if(sideBar.style.display == "block" && sideBar.style.right == "0px"){
-//         sideBar.style.display = "none";
-//         sideBar.style.right = "-100px"
-//     } else {
-//         return false;
-//     }
-    
-//     sideBar.style.display = "none";
-//     sideBar.style.right = "-100px";
-//     console.log(true);
-// });
-
-window.addEventListener('scroll', function(){
+window.addEventListener('scroll', function () {
     // console.log(inner.scrollWidth)
 });
 
